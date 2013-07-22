afc683400ab1d197657a9aba16a32df43c052fe1
Alex Hinton <alex.hinton@rbi.co.uk>
2013-07-22 17:04:16 +0100

    added css styling update filter logic

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,22 +9,14 @@ var myApp = angular.module('myApp.controllers', []).
 
     }]);
 
-myApp.factory('FDAFactory', function ($http) {
-    return $http.get('http://api.flightgloballocal.rbidev.ds/v1/Fleet/Alerts?apiToken=00000000-0000-0000-0000-000000000001&skip=0&take=10');
-});
-
-myApp.controller('FDAController', ['$scope', '$http', '$timeout', 'FDAFactory', function ($scope, $http, $timeout, FDAFactory) {
-    //    FDAFactory.success(function(data) {
-    //        $scope.FDAs = data.Events;
-    //});
-
+myApp.controller('FDAController', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
     $scope.FilterFDAs = function(filterTerm) {
-        $scope.xx = filterTerm;
+        $scope.searchTerms = { Type: filterTerm };
     };
 
     var latestDate = '2005-11-09T00:00:00';
     var GetStuff = function(http) {
-        return http.get('http://api.flightgloballocal.rbidev.ds/v1/Fleet/Alerts?apiToken=00000000-0000-0000-0000-000000000001&skip=0&take=10&earliestDate=' + latestDate);
+        return http.get('http://api.flightgloballocal.rbidev.ds/v1/Fleet/Alerts?apiToken=00000000-0000-0000-0000-000000000001&skip=0&take=100&earliestDate=' + latestDate);
     };
 
 
@@ -36,7 +28,7 @@ myApp.controller('FDAController', ['$scope', '$http', '$timeout', 'FDAFactory', 
                 latestDate = data.Events[0].EventDate;
             }
             
-            $timeout(tick, 15000);
+            $timeout(tick, 30000);
         });
     })();
 }]);
