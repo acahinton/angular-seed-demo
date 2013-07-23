3c557447155185103c2a467e318dc8a93abfe54d
Gareth Jones <Gareth.Jones@rbi.co.uk>
2013-07-23 14:23:26 +0100

    A better directive for the show/hide delivery date

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,19 +7,24 @@ var myApp = angular.module('myApp.controllers', []).
         $scope.message = "From a controller";
     }]);
 
-myApp.directive('showhideonhover', function() {
-    return function(scope, element, attrs) {
-        element.bind("mouseenter", function () {
-            var item = element.children(0);
-            if (item[0].innerText != '') {
-                item.addClass('visibleAtLast');
-            }
-        });
+myApp.directive('fleetalertdate', function () {
+    return {
+        restrict: "E",
+        replace: true,
+        scope: {
+            eventdate: '@'
+        },
+        template: "<span class=\"deliveryDate\">{{eventdate}}</span>",
+        link: function(scope, element) {
 
-        element.bind("mouseleave", function () {
-            var item = element.children(0);
-            item.removeClass('visibleAtLast');
-        });
+            element.parent().bind("mouseenter", function () {
+                element.addClass('visibleAtLast');
+            });
+
+            element.parent().bind("mouseleave", function () {
+                element.removeClass('visibleAtLast');
+            });
+        }
     };
 });
 
@@ -31,7 +36,7 @@ myApp.controller('FDAController', ['$scope', '$http', '$timeout', function ($sco
 
     var latestDate = '2005-11-09T00:00:00';
     var GetStuff = function(http) {
-        return http.get('http://api.flightgloballocal.rbidev.ds/v1/Fleet/Alerts?apiToken=00000000-0000-0000-0000-000000000001&skip=0&take=100&earliestDate=' + latestDate);
+        return http.get('http://api.flightgloballocal.rbidev.ds/v1/Fleet/Alerts?apiToken=00000000-0000-0000-0000-000000000001&skip=0&take=10&earliestDate=' + latestDate);
     };
 
 
